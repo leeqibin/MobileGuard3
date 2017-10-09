8927f5e61c0d4589afe63d48a7839f4914883cb0
student <1025559155@qq.com>
2017-10-09 09:28:33 +0800

    最新修改

--- app/src/main/java/com/me/android/mobileguard/SplashActivity.java
+++ app/src/main/java/com/me/android/mobileguard/SplashActivity.java
@@ -9,17 +9,16 @@ import com.me.android.mobileguard.m1home.utils.VersionUpdateUtils;
 
 public class SplashActivity extends AppCompatActivity {
     private TextView mTvVersion;
-    private  String mVersion;
+    private String mVersion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        mVersion = MyUtils.getVersion(getApplicationContext());
-        mTvVersion = (TextView) findViewById(R.id.tv_splash_version);
-        mTvVersion.setText("版本号："+ mVersion );
-        final VersionUpdateUtils versionUpdateUtils = new VersionUpdateUtils(mVersion,SplashActivity.this);
+        mVersion= MyUtils.getVersion(getApplicationContext());
+        mTvVersion=(TextView)findViewById(R.id.tv_splash_version);
+        mTvVersion.setText("版本号："+mVersion);
+        final VersionUpdateUtils versionUpdateUtils=new VersionUpdateUtils(mVersion,SplashActivity.this);
         new Thread(){
-            @Override
             public void run(){
                 super.run();
                 versionUpdateUtils.getCloudVersion();
